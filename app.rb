bd446d8cf449e8d005fd420599ebbec2e51b7176
github username <github email address>
2019-02-20 17:06:24 -0800

    First commit.

--- app.rb
+++ app.rb
@@ -2,4 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get('/') {
+    erb :index
+  }
+
+  get('/new') {
+    erb :create_puppy
+  }
+
+  post('/puppy') {
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = [params[:age], "months"].join(" ")
+    erb :display_puppy
+  }
 end
