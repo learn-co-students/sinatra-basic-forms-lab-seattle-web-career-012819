89277ec50a87880ec875bdc72a7e73008abe51f7
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-02-20 17:08:11 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    attr = params.each_with_object({}) do |(k,v), h|
+      h[k.to_sym] = v
+    end
+    @new_puppy = Puppy.new(attr)
+    erb :display_puppy
+  end
 end
